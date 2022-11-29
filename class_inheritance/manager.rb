e3a03e3574426f134760175c9ba33ad9549411ad
zahiahmed <zahi.ahmed33@gmail.com>
2022-11-29 15:51:34 -0500

    finished class_inheritance

--- class_inheritance/manager.rb
+++ class_inheritance/manager.rb
@@ -1,5 +1,5 @@
 require_relative "employee"
-
+require "byebug"
 class Manager < Employee
     attr_reader :employees
     def initialize(name, title, salary, boss)
@@ -8,17 +8,22 @@ class Manager < Employee
     end
 
     def add_employee(employee)
-        if self == employee.boss
+        # p self
+        # p employee.boss
+        # if self == employee.boss
             @employees << employee
-        end 
+        # end 
     end
 
     def bonus(multiplier)
         total_salary = 0
         self.employees.each do |employee|
-            total += employee.salary
+            if employee.is_a?(Manager)
+                total_salary += employee.salary * multiplier
+            end
+            total_salary += employee.bonus(multiplier)
         end
-        total_salary * multiplier
+        total_salary
     end
 end
 
@@ -26,10 +31,17 @@ ned = Manager.new("Ned", 1000000, "Founder", nil)
 darren = Manager.new("Darren", 78000, "TA Manager", "Ned")
 shawna = Employee.new("Shawna", 12000, "TA", "Darren")
 david = Employee.new("David", 10000, "TA", "Darren")
+# p ned.salary
+# p darren.salary
+# p shawna.salary
+# p david.salary
 
 ned.add_employee(darren)
 darren.add_employee(shawna)
 darren.add_employee(david)
+
+ned.employees
+darren.employees
 
 p ned.bonus(5) # => 500_000
 p darren.bonus(4) # => 88_000

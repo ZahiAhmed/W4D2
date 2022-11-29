839078bf24b47431963a145fb9badd06f839e0ed
Wilson Wu <wuyecheng738@gmail.com>
2022-11-29 15:17:54 -0500

    testing manager cases

--- class_inheritance/employee.rb
+++ class_inheritance/employee.rb
@@ -1,14 +1,15 @@
 class Employee
     attr_accessor :name, :title, :salary, :boss
-    def initialize(name, title, salary, boss)
+    def initialize(name, salary, title, boss)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
     end
 
-    def bonus(multiplier)
-        (employee.salary) * multiplier
-    end
 
+    def bonus(multiplier)
+        (self.salary) * multiplier
+    end
 end
+
--- class_inheritance/manager.rb
+++ class_inheritance/manager.rb
@@ -3,8 +3,14 @@ require_relative "employee"
 class Manager < Employee
     attr_reader :employees
     def initialize(name, title, salary, boss)
-        super()
+        super
         @employees = []
+    end
+
+    def add_employee(employee)
+        if self == employee.boss
+            @employees << employee
+        end 
     end
 
     def bonus(multiplier)
@@ -15,3 +21,16 @@ class Manager < Employee
         total_salary * multiplier
     end
 end
+
+ned = Manager.new("Ned", 1000000, "Founder", nil)
+darren = Manager.new("Darren", 78000, "TA Manager", "Ned")
+shawna = Employee.new("Shawna", 12000, "TA", "Darren")
+david = Employee.new("David", 10000, "TA", "Darren")
+
+ned.add_employee(darren)
+darren.add_employee(shawna)
+darren.add_employee(david)
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
